9ebb135b16f8c2768759562d333851bdc15376e6
Javier Gamarra <nhpatt@gmail.com>
2017-01-25 08:04:20 -0800

    render 1 element

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,10 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  talk:number = 2
+  story = {
+    "created_at": "2017-01-25T15:50:39.000Z",
+    "title": "Make Learning a Lifelong Habit",
+    "url": "https://hbr.org/2017/01/make-learning-a-lifelong-habit",
+    "author": "devy",
+    "points": 1,
+    "story_text": null,
+    "comment_text": null,
+    "num_comments": 0,
+    "story_id": null,
+    "story_title": null,
+    "story_url": null,
+    "parent_id": null,
+    "created_at_i": 1485359439,
+    "_tags": [
+      "story",
+      "author_devy",
+      "story_13482334"
+    ],
+    "objectID": "13482334",
+    "_highlightResult": {
+      "title": {
+        "value": "Make Learning a Lifelong Habit",
+        "matchLevel": "none",
+        "matchedWords": []
+      },
+      "url": {
+        "value": "https://hbr.org/2017/01/make-learning-a-lifelong-habit",
+        "matchLevel": "none",
+        "matchedWords": []
+      },
+      "author": {
+        "value": "devy",
+        "matchLevel": "none",
+        "matchedWords": []
+      }
+    }
+  }
 
   constructor(public navCtrl: NavController) {
-    setTimeout(x => this.talk = 4, 2000);
   }
 
 }
